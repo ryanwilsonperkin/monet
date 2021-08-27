df12403da8099a7b44f90a12c7b5d1b1d7fd6412
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2021-08-27 17:54:27 -0400

    Transactions are completely readonly

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,5 +1,5 @@
 class TransactionsController < ApplicationController
-  before_action :set_transaction, only: %i[ show edit update destroy ]
+  before_action :set_transaction, only: :show
 
   # GET /transactions or /transactions.json
   def index
@@ -10,34 +10,9 @@ class TransactionsController < ApplicationController
   def show
   end
 
-  # GET /transactions/new
-  def new
-    @transaction = Transaction.new
-  end
-
-  # POST /transactions or /transactions.json
-  def create
-    @transaction = Transaction.new(transaction_params)
-
-    respond_to do |format|
-      if @transaction.save
-        format.html { redirect_to @transaction, notice: "Transaction was successfully created." }
-        format.json { render :show, status: :created, location: @transaction }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @transaction.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_transaction
       @transaction = Transaction.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def transaction_params
-      params.require(:transaction).permit(:credit_card_statement_id, :date, :description, :debit, :credit, :balance)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :transactions, except: [:edit, :update, :destroy]
+  resources :transactions, only: [:index, :show]
   resources :credit_card_statements, except: [:edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
