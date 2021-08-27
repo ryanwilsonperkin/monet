5db2e019000f9285728bd594a87f2adc4536743e
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2021-08-27 13:33:18 -0400

    Numeric validators

--- app/models/credit_card_statement.rb
+++ app/models/credit_card_statement.rb
@@ -1,3 +1,5 @@
 class CreditCardStatement < ApplicationRecord
   validates :content, :year, :month, presence: true
+  validates :year, numericality: { greater_than: 1900, less_than: 3000 }
+  validates :month, numericality: { greater_than: 0, less_than: 13 }
 end
