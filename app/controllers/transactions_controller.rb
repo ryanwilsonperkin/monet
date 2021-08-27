5804dce991ab631110b53f2f8227efd9ac267892
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2021-08-27 17:35:12 -0400

    No destroying transactions
    You have to destroy the whole credit card statement

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -30,15 +30,6 @@ class TransactionsController < ApplicationController
     end
   end
 
-  # DELETE /transactions/1 or /transactions/1.json
-  def destroy
-    @transaction.destroy
-    respond_to do |format|
-      format.html { redirect_to transactions_url, notice: "Transaction was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_transaction
