1f0503424016250dc3741eccdb10748010e4bb79
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2021-08-27 16:41:50 -0400

    Destroy dependent transactions

--- app/models/credit_card_statement.rb
+++ app/models/credit_card_statement.rb
@@ -1,7 +1,7 @@
 require 'csv'
 
 class CreditCardStatement < ApplicationRecord
-  has_many :transactions
+  has_many :transactions, dependent: :destroy
   validates :content, :year, :month, presence: true
   validates :year, numericality: { greater_than: 1900, less_than: 3000 }
   validates :month, numericality: { greater_than: 0, less_than: 13 }
