4b161841902fc5e940d9dbc7c264a9d23dbb47d2
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2021-08-29 17:43:13 -0400

    Automatically assign vendor ids where possible

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,5 +2,21 @@ class Transaction < ApplicationRecord
   belongs_to :credit_card_statement
   belongs_to :vendor, optional: true
 
+  before_create :auto_assign_vendor!
+
   delegate :name, to: :vendor, prefix: true, allow_nil: true
+
+  private
+
+  def auto_assign_vendor!
+    return if vendor_id
+
+    other = Transaction
+      .where(description: description)
+      .where.not(vendor_id: nil)
+      .first
+    return unless other
+
+    self.vendor_id = other.vendor_id
+  end
 end
