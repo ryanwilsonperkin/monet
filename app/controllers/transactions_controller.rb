566a7cb4d9d201f44f6a7ccee6e4bbb91668de52
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2021-08-28 22:58:06 -0400

    Add vendor to transaction

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,5 +1,5 @@
 class TransactionsController < ApplicationController
-  before_action :set_transaction, only: :show
+  before_action :set_transaction, only: [:show, :edit, :update]
 
   # GET /transactions or /transactions.json
   def index
@@ -11,6 +11,24 @@ class TransactionsController < ApplicationController
 
   # GET /transactions/1 or /transactions/1.json
   def show
+  end
+
+  # GET /transactions/1/edit
+  def edit
+  end
+
+  # PATCH/PUT /transactions/1 or /transactions/1.json
+  def update
+    transaction_params = params.require(:transaction).permit(:vendor_id)
+    respond_to do |format|
+      if @transaction.update(transaction_params)
+        format.html { redirect_to @transaction, notice: "Transaction was successfully updated." }
+        format.json { render :show, status: :ok, location: @transaction }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @transaction.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :vendors
-  resources :transactions, only: [:index, :show]
+  resources :transactions, except: [:new, :create, :delete]
   resources :credit_card_statements, except: [:edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
