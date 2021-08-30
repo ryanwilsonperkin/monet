ed90a94f19cceb2638ed836c4fd4780d81a0a736
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2021-08-29 20:10:47 -0400

    Add route for reports/

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'reports#index'
   namespace :reports do
+    root action: 'index'
     get 'monthly', as: 'monthly_redirect', status: 302, to: redirect { "reports/monthly/#{Date.current.year}/#{Date.current.month}" }
     get 'monthly/:year/:month', action: 'monthly', as: 'monthly'
     get 'yearly', as: 'yearly_redirect', status: 302, to: redirect { "reports/yearly/#{Date.current.year}" }
