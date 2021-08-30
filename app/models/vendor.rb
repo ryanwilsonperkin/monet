180dae6ba89383adc82bca5e1de7a2db9254e2c0
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2021-08-30 14:48:12 -0400

    Add vendor category to list/show pages

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,4 +1,6 @@
 class Vendor < ApplicationRecord
   has_many :transactions, dependent: :nullify
   belongs_to :category, optional: true
+
+  delegate :name, to: :category, prefix: true, allow_nil: true
 end
